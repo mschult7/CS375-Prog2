e3d3f136a6121c3bb8eaaa60e53cab047d68dac0
Matthew Schultz <mschult7@bingha>
2020-04-14 15:30:39 -0400

    other sort

--- BestFirstSearch.cpp
+++ BestFirstSearch.cpp
@@ -57,7 +57,7 @@ int main(int argc, char * argv[]){
   int temp_weight;
   for(int l=0; l < numItems; l++){
     for(int x=1; x < (numItems-l); x++){
-      if(p.at(x-1)/w.at(x-1) > p.at(x)/w.at(x)){
+      if(p.at(x-1)/w.at(x-1) < p.at(x)/w.at(x)){
         temp_price = p.at(x-1);
         temp_weight = w.at(x-1);
         p.at(x-1) = p.at(x);
